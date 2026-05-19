0fa231d83aa2827664d3850cc7099e044348441b
Eaviyi <eavidy@qq.com>
2026-05-19 22:55:37 +0800

    fix: 简化退出登录逻辑，移除异常处理并保持重定向

--- fe/src/routes/logout.tsx
+++ fe/src/routes/logout.tsx
@@ -3,15 +3,7 @@ import { api } from "@/request/client.ts";
 
 export const Route = createFileRoute("/logout")({
   async beforeLoad(ctx) {
-    try {
-      await api["/passport/logout"].post();
-    } catch (e) {
-      if (globalThis.history.length > 1) {
-        globalThis.history.back();
-        throw redirect({ to: "/login", search: ctx.search });
-      }
-      return;
-    }
+    await api["/passport/logout"].post();
     throw redirect({ to: "/login", search: ctx.search });
   },
   validateSearch(value): { redirect?: string } {
